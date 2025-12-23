0750bbcea329f3b412e2fb5d2f15242c31ff34bd
jeongmin <skawjdals96@gmail.com>
2025-12-23 17:57:18 +0900

    first commit

---
+++ src/test/java/com/jeongminkim_backend/JeongminkimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongminkim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongminkimBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
